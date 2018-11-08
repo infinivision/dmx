376e0abea59e94028827349cc722c4642c494ae5
changyue.zhou <23199412@qq.com>
2018-11-08 13:51:54 +0800

    delete all

--- api/src/main/java/com/dmx/api/dao/TTagMetaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dmx.api.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-import com.dmx.api.entity.TTagMetaEntity;
-
-public interface TTagMetaRepository extends CrudRepository<TTagMetaEntity, Long>  {
-}
